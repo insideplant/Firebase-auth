d679b7dc69c434cb8201a9b21b55a51d050e672e
insideplant <kami.10pearl@gmail.com>
2021-12-12 02:53:57 +0900

    delete env

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { app } from "../base.js";
 
-// contextの作成
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
-  // ユーザーをログインさせる関数
   const login = async (email, password, history) => {
     try {
       await app.auth().signInWithEmailAndPassword(email, password);
@@ -17,7 +15,6 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  // 新しいユーザーを作成しログインさせる関数
   const signup = async (email, password, history) => {
     try {
       await app.auth().createUserWithEmailAndPassword(email, password);
@@ -28,11 +25,10 @@ export const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    app.auth().onAuthStateChanged(setCurrentUser);
+    app.auth().onAuthStateChanged((user) => setCurrentUser(user));
   }, []);
 
   return (
-    // Contextを使用して認証に必要な情報をコンポーネントツリーに流し込む。
     <AuthContext.Provider
       value={{
         login: login,
